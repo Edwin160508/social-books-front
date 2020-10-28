dba9121b4937986774bc53040e94fd02e614b359
Lima <edwin.lima@capgemini.com>
2020-10-28 10:15:47 -0300

    Mudando grid e inputs para primeNG,

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AutorComponent } from './autor/autor.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component';
+/* Modulos do Primng*/
+import { ButtonModule, InputTextModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [AutorComponent, PaginaNaoEncontradaComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    /*PrimeNG*/
+    ButtonModule,
+    InputTextModule
   ]
 })
 export class CoreModule { }
